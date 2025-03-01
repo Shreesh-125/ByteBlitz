80865d5c3d2d81b37426f5bfaf569f0d9d259bda
Enum2 <sujalbs2018@gmail.com>
2025-03-01 23:47:23 +0530

    schema problem,contests add

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    fullname:{
-        type:String,
-        required:true
-    }
-})
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+});
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
