33b87201b58fa6a368851608f8da33890ecacca5
Shreesh-125 <shreeshpolawar@gmail.com>
2025-03-01 23:50:18 +0530

    added user and admin schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,24 +1,109 @@
 import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  submissions: [
+    {
+      questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'problems',
+        required: true,
+      },
+      code: {
+        type: String,
+        required: true,
+      },
+      status: {
+        type: String,
+        enum: ["Accepted", "Rejected", "Pending"],
+        required: true,
+      },
+      date: {
+        type: String,
+        default: () => {
+          const now = new Date();
+          const options = {
+            month: "short",
+            day: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          };
+          return now.toLocaleString("en-US", options).replace(",", "");
+        },
+        required: true,
+      },
+      language: {
+        type: String,
+        enum: ["c++", "java", "python"],
+        required: true,
+      },
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+  ],
+  rating: {
+    type: String,
+    default: 0,
+  },
+  problem_solved: [
+    {
+      date: {
+        type: String,
+        default: () => {
+          const now = new Date();
+          const day = String(now.getDate()).padStart(2, "0");
+          const month = String(now.getMonth() + 1).padStart(2, "0");
+          const year = now.getFullYear();
+          return `${day}/${month}/${year}`;
+        },
+        required: true,
+      },
+      problemSolved: {
+        type: Number,
+        required: true,
+      },
     },
-    password:{
-        type:String,
-        required:true,
+  ],
+  contests: [
+    {
+      contestId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "contests",
+      },
+      rank: {
+        type: Number,
+        required: true,
+      },
+      ratingChange: {
+        type: Number,
+        required: true,
+      },
+      newRating: {
+        type: Number,
+        required: true,
+      },
     },
-    fullname:{
-        type:String,
-        required:true
-    }
-})
+  ],
+  max_rating: {
+    type: Number,
+    default: 0,
+  },
+  location: {
+    type: String,
+  },
+});
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
