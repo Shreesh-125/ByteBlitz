593759fa32077a773d106380bf00084a9abff75e
ambrollins619 <keshavmahansaria@gmail.com>
2025-03-15 11:03:16 +0530

    Problems List and Problem with Code Editor Page Done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss(),
+    react()
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
