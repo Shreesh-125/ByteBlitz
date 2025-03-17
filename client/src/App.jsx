bbd047d240a6792d34e503d3eae7754a783549e6
manugrover <grovermanuj2005@gmail.com>
2025-03-17 17:29:18 +0530

    Home page, blog page, standings, contest's problems page, overall submission page, profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,34 @@
-import { Route, Routes } from "react-router-dom";
-import Contests from "./pages/Contests";
-import Ranking from "./pages/Ranking";
-import Problems from "./pages/Problems";
-import Home from "./pages/Home";
-import Navbar from "./ui/Navbar";
-import Problempage from "./pages/Problempage";
-import ProblemAllSubmissionsPage from "./pages/ProblemAllSubmissionsPage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './ui/Navbar';
+import Profilepage from './pages/Profilepage'
+import Allsubmissionpage from './pages/Allsubmissionpage';
+import Conteststandingpage from './pages/Conteststandingpage';
+import Home from './pages/Home';
+import Contestproblempage from './pages/Contestproblempage';
+import Particularblog from './pages/Particularblog';
+import Allblogs from './pages/Allblogs';
+const App = () => {
 
-
-function App() {
   return (
-    <>
-      <Navbar/>
+    <Router>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/contests' element={<Contests/>}/>
-        <Route path='/ranking' element={<Ranking/>}/>
-        <Route path='/problems' element={<Problems/>}/>
-        <Route path='/problems/20' element={<Problempage/>}/>
-        <Route path='/problems/20/submissions' element={<ProblemAllSubmissionsPage/>}/>
+        <Route path="/*" element={<Home/>} />
+        <Route path="/contests" element={<Contestproblempage/>} />
+        <Route path="/problems" element={<Contestproblempage/>} />
+        <Route path="/ranking" element={<Allsubmissionpage/>} />
+        <Route path="/blogs" element={<Allblogs/>} />
+        <Route path="/submissions" element={<Home/>} />
+        <Route path="/profile" element={<Profilepage/>} />
+        <Route path="/My_Friends" element={<Profilepage/>} />
+        <Route path="/settings" element={<Profilepage/>} />
+        <Route path="/standings" element={<Conteststandingpage />} />
+        <Route path="/contestproblems" element={<Contestproblempage/>} />
+        <Route path="/blogs/:id" element={<Particularblog />} />
       </Routes>
-    </>
+    </Router>
   );
-}
+};
 
 export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Announcements from '../ui/Announcements'
+import styles from '../styles/Home.module.css'
+import Homesidebar from './Homesidebar'
+import {Link} from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom";
+import Particularblog from './Particularblog'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={styles.home}>
+      <Announcements />
+      <Homesidebar/>
+    </div>
   )
 }
 
