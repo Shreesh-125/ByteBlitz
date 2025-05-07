dd2ff5c9d1b8de9aaaeb0b2693731e0ce5934cde
Shreesh-125 <shreeshpolawar@gmail.com>
2025-05-07 11:07:28 +0530

    minor changes

--- server/route/contest.route.js
+++ server/route/contest.route.js
@@ -31,6 +31,6 @@ export const contestRoutes = (io) => {
   router.route("/:contestId/problems").get(getContestProblem);
   router.route("/checkregistered").post(isRegisteredInContest)
   router.route("/getStatus/:contestId").get(getContestStatus);
-  router.route("/:contestId/leaderboard").get(isAuthenticated,getLeaderBoard);
+  router.route("/:contestId/leaderboard").get(getLeaderBoard);
   return router;
 };
